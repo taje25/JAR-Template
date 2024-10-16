e0306ef7bb45bd09e4c52d2fd0b3bb2ca0a7daff
taje25 <taje@potomacschool.org>
2024-10-16 16:11:05 -0400

    initial commit

---
+++ src/robot-config.cpp
@@ -0,0 +1,23 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen.
+brain  Brain;
+
+//The motor constructor takes motors as (port, ratio, reversed), so for example
+//motor LeftFront = motor(PORT1, ratio6_1, false);
+
+//Add your devices below, and don't forget to do the same in robot-config.h:
+extern smartdrive drivetrain;
+extern controller Controller1;
+extern motor intake;
+extern digital_out clamp;
+extern motor arm;
+
+
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
